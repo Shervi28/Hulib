46bd627bb5c88a2db2ff2ce9bdd0cce2ff8401bd
Shervin the Prodigy <68794773+Shervi28@users.noreply.github.com>
2022-05-26 14:55:12 -0700

    Create routes.js

---
+++ src/router/routes.js
@@ -0,0 +1,62 @@
+
+
+const routes = [
+
+  {
+
+    path: '/',
+
+    component: () => import('layouts/MainLayout.vue'),
+
+    children: [
+
+      { path: '/', component: () => import('pages/Auth.vue') },
+
+      {
+
+        path: '/home',
+
+        component: () => import('pages/PageHome.vue'),
+
+        name: 'Home',
+
+        meta: {requiresAuth: true}
+
+      },
+
+      { 
+
+        path: '/about',
+
+        component: () => import('pages/PageAbout.vue'),
+
+        name: 'About',
+
+        meta: {requiresAuth: true}
+
+      }
+
+    ]
+
+  },
+
+
+
+  // Always leave this as last one,
+
+  // but you can also remove it
+
+  {
+
+    path: '*',
+
+    component: () => import('pages/Error404.vue')
+
+  }
+
+]
+
+
+
+export default routes
+
